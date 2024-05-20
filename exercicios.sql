6ab01c1e6d7f180b854a59ddd1f464c8f88efa9d
CaiqueBAR <159191424+CaiqueBAR@users.noreply.github.com>
2024-05-20 08:47:16 -0300

    Add files via upload

---
+++ exercicios.sql
@@ -0,0 +1,25 @@
+use bdex3_redesocial;
+
+select NOME_USUARIO, SOBRENOME, EMAIL  from usuarios;
+
+#3. Listar os nomes e descrições dos grupos.
+
+SELECT NOME_GRUPO, DESCRICAO FROM GRUPOS;
+
+#4. Exibir os detalhes das postagens: ID da postagem, texto, data e hora, e o nome do usuário que postou.
+
+SELECT ID_POST, TEXTO, DATA_HORA, USUARIO FROM postagens;
+
+#5. Listar o nome dos usuários e a quantidade de postagens que cada um fez.
+
+# ID_POST | TEXTO IMAGEM | DATA_HORA | USUARIO
+#ID_USUARIO | NOME_USUARIO |
+
+SELECT
+ 
+
+
+
+
+
+
